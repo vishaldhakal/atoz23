8e465fd6c6085515fd7f76591f2d7c98e1205632
vishaldhakal <vishaldhakal96@gmail.com>
2023-10-07 07:24:47 +0545

    sDc

--- gifts/views.py
+++ gifts/views.py
@@ -100,110 +100,106 @@ def downloadDataToday(request):
     return response
 
 
+from datetime import date
+from django.shortcuts import render, redirect
+from .models import Customer, Sales, Offers, Gift, FixOffer
+
 def registerCustomer(request):
     if request.method == "POST":
-        customer_name = request.POST["customer_name"]
-        contact_number = request.POST["phone_number"]
-        shop_name = request.POST["shop_name"]
-        product_name = request.POST["product_name"]
-        how_know_about_campaign = request.POST["how_know_about_campaign"]
+        customer_name = request.POST.get("customer_name")
+        contact_number = request.POST.get("phone_number")
+        shop_name = request.POST.get("shop_name")
+        product_name = request.POST.get("product_name")
+        how_know_about_campaign = request.POST.get("how_know_about_campaign")
 
-        custt = Customer.objects.filter(
+        # Check if the customer already exists
+        existing_customer = Customer.objects.filter(
             customer_name=customer_name,
             phone_number=contact_number,
             shop_name=shop_name,
             product_name=product_name,
-            how_know_about_campaign=how_know_about_campaign
-        )
+            how_know_about_campaign=how_know_about_campaign,
+        ).first()
 
-        if custt:
+        if existing_customer:
             return redirect('index')
 
+        # Create a new customer
         customer = Customer.objects.create(
             customer_name=customer_name,
             phone_number=contact_number,
             shop_name=shop_name,
             product_name=product_name,
             sale_status="SOLD",
-            how_know_about_campaign=how_know_about_campaign
+            how_know_about_campaign=how_know_about_campaign,
         )
-        customer.save()
+
+        # Select Gift
         giftassign = False
-
-        """ Select Gift """
         today_date = date.today()
-        offers_all = Offers.objects.filter(date_valid=today_date)
-        sales_all = Sales.objects.all()
-        check = 0
-        for sale in sales_all:
-            if sale.date == today_date:
-                check = 1
-        if check == 0:
-            saless = Sales.objects.create(sales_count=0, date=today_date)
-            saless.save()
 
-        sale_today = Sales.objects.get(date=today_date)
+        # Ensure there's a Sales record for today
+        sale_today, created = Sales.objects.get_or_create(
+            date=today_date,
+            defaults={'sales_count': 0}
+        )
         get_sale_count = sale_today.sales_count
-        sale_today.sales_count = get_sale_count + 1
+        sale_today.sales_count +=1
         sale_today.save()
 
-        dsd = FixOffer.objects.all()
-        myoff = False
+        if not created:
+            sale_today.sales_count += 1
+            sale_today.save()
 
-        for off in dsd:
-            if (contact_number in off.phone_number):
-                if (off.quantity > 0):
-                    customer.gift = off.gift
-                    customer.save()
-                    giftassign = True
-                    myoff = True
-                    off.quantity = 0
-                    off.save()
-                    break
+        # Check if the customer has a specific gift offer (FixOffer)
+        fix_offer = FixOffer.objects.filter(
+            phone_number=contact_number, quantity__gt=0
+        ).first()
 
-        found = False
-
-        if myoff == False:
-            while found == False:
-                getofff = Offers.objects.filter(date_valid=today_date).order_by("?").first()
-                if getofff and getofff.quantity > 0:
-                    if getofff.type_of_offer == "After every certain sale":
-                        if (getofff.quantity > 0):
-                            """ Grant Gift """
-                            qty = getofff.quantity
-                            customer.gift = getofff.gift
-                            customer.save()
-                            getofff.quantity = qty - 1
-                            getofff.save()
-                            giftassign = True
-                            found = True
-                            break
-                    else:
-                        if (getofff.quantity > 0):
-                            """ Grant Gift """
-                            qty = getofff.quantity
-                            customer.gift = getofff.gift
-                            customer.save()
-                            getofff.quantity = qty - 1
-                            getofff.save()
-                            giftassign = True
-                            found = True
-                            break
+        if fix_offer:
+            customer.gift = fix_offer.gift
+            customer.save()
+            giftassign = True
+            fix_offer.quantity = 0
+            fix_offer.save()
 
         if not giftassign:
-            weekly_offers = Offers.objects.filter(
-                date_valid__lte=today_date, date_valid__gte=today_date, type_of_offer="Weekly Offer"
-            )
+            for offer in Offers.objects.filter(date_valid=today_date,type_of_offer="After every certain sale"):
+                if offer.type_of_offer == "After every certain sale":
+                    if (((get_sale_count + 1) % int(offer.offer_condition_value) == 0)) and (offer.quantity > 0):
+                        qty = offer.quantity
+                        customer.gift = offer.gift
+                        customer.save()
+                        offer.quantity = qty - 1
+                        offer.save()
+                        giftassign = True
+                        break
+                if offer.type_of_offer == "At certain sale position":
+                    if ((get_sale_count + 1) == int(offer.offer_condition_value)) and (offer.quantity > 0):
+                        qty = offer.quantity
+                        customer.gift = offer.gift
+                        customer.save()
+                        offer.quantity = qty - 1
+                        offer.save()
+                        giftassign = True
+                        break
 
-            for offer in weekly_offers:
-                if (get_sale_count + 1) in offer.sale_numbers and offer.quantity > 0:
-                    qty = offer.quantity
-                    customer.gift = offer.gift
-                    customer.save()
-                    offer.quantity = qty - 1
-                    offer.save()
-                    giftassign = True
-                    break
+        if not giftassign:
+            # Find a weekly offer based on the number of sales
+            weekly_offers = Offers.objects.filter(
+                date_valid__lte=today_date,
+                date_valid__gte=today_date,
+                type_of_offer="Weekly Offer",
+                sale_numbers__contains=[sale_today.sales_count + 1],
+                quantity__gt=0
+            ).first()
+
+            if weekly_offers:
+                customer.gift = weekly_offers.gift
+                customer.save()
+                weekly_offers.quantity -= 1
+                weekly_offers.save()
+                giftassign = True
 
         return render(request, "output.html", {"customer": customer, "giftassigned": giftassign})
     else:
