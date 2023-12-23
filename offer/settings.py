f3d245160516c501f96f76b222995391d588c839
vishaldhakal <vishaldhakal96@gmail.com>
2023-12-23 13:52:54 +0545

    homepage design change

--- offer/settings.py
+++ offer/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'offer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'atoz',
@@ -87,14 +87,14 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }
-}
+} """
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
+}
 
 
 # Password validation
